6c422e1ec7642980039188fbf60d4aa73ee86066
Keval <kevaldodiya@gmail.com>
2021-08-18 16:29:59 +0530

    "second commit"

--- TestGit1/src/com/ofss/Main.java
+++ TestGit1/src/com/ofss/Main.java
@@ -1,10 +1,14 @@
 package com.ofss;
 
 public class Main {
-
+	
+	public static int fun1(int x, int y) {
+		return x*y;
+	}
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello brother");
+		System.out.println(fun1(5,10));
 	}
 
 }
